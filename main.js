972e6e5cd00975473d6e3b00b85bd6c00d59999a
K Sam <81320998+K-Sam1005@users.noreply.github.com>
2022-04-01 12:54:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+harry_potter = "";
+peter_pan = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+function preload(){
+    harry_potter = loadSound("harry_potter.mp3");
+    peter_pan = loadSound("peter_pan.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(650,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotResults);
+}
+
+function modelLoaded(){
+    console.log("PoseNet is initialized");
+}
+
+function gotResults(results){
+    if(results.length > 0){
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+    }
+}
+
+function draw(){
+    image(video, 0, 0, 650, 500);
+}
